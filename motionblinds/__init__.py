38c17a6cb5999fe8d2fb319e1c34b448c531ffb7
starkillerOG <starkiller.og@gmail.com>
2021-10-17 15:00:37 +0200

    push to version 0.5.7

--- motionblinds/__init__.py
+++ motionblinds/__init__.py
@@ -11,7 +11,7 @@ import logging
 logging.getLogger(__name__)
 
 __title__ = "motion-blinds"
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 
 # Import motion_blinds module
 from .motion_blinds import MotionGateway
