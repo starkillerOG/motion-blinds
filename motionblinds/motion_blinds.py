f03311ae2c44a8d567e318b1b6cf81d7ab5fc71e
starkillerOG <starkiller.og@gmail.com>
2022-05-13 12:16:27 +0200

    Do not restore angle if fully closing

--- motionblinds/motion_blinds.py
+++ motionblinds/motion_blinds.py
@@ -1296,7 +1296,7 @@ class MotionBlind:
         if angle is not None:
             target_angle = round(angle * self._max_angle / 180.0, 0)
             data["targetAngle"] = target_angle
-        if restore_angle and self.angle is not None:
+        if restore_angle and self.angle is not None and position != 0:
             target_angle = round(self.angle * self._max_angle / 180.0, 0)
             data["targetAngle"] = target_angle
 
